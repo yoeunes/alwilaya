01e398b9a4262f6079c8f67719ca929fb833d70d
fatima ezzahra hadadi <fatima.ezzahrae.hadadi@gmail.com>
2016-08-11 14:16:50 +0200

    creer table des settings

---
+++ database/migrations/2016_08_11_120546_create_setting_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSettingTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+
+            $table->increments('id');
+            $table->string('name');
+            $table->string('display_name');
+            $table->string('value');
+            $table->mediumText('description');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('settings');
+    }
+}
