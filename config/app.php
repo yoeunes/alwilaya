06cc8b86627084589b0f3e5970bd3844a33efb71
Younes Khoubza <younes.khoubza@gmail.com>
2016-08-11 20:10:42 +0100

    laravel_collective

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Zizaco\Entrust\EntrustServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
 
     ],
 
@@ -213,6 +214,8 @@ return [
         'View'      => Illuminate\Support\Facades\View::class,
 
         'Entrust' => Zizaco\Entrust\EntrustFacade::class,
+        'Form'    => Collective\Html\FormFacade::class,
+        'Html'    => Collective\Html\HtmlFacade::class,
     ],
 
 ];
