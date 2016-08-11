b6aa55f03e62e20ab532795b5813204d690a27d9
fatima ezzahra hadadi <fatima.ezzahrae.hadadi@gmail.com>
2016-08-11 12:08:46 +0200

    creer table de reclamationtype soit orale ou par email

---
+++ database/migrations/2016_08_11_100221_create_reclamationType_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReclamationTypeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reclamationTypes', function (Blueprint $table) {
+
+            $table->increments('id');
+            $table->string('name');
+            $table->string('display_name');
+            $table->mediumText('description');
+            $table->timestamps();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('reclamationTypes');
+    }
+}
