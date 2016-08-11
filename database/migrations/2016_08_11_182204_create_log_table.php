8f21bf206cdbd0c98f4530e03acf8e7db97234f2
fatima ezzahra hadadi <fatima.ezzahrae.hadadi@gmail.com>
2016-08-11 20:30:12 +0200

    -acrre

---
+++ database/migrations/2016_08_11_182204_create_log_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLogTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->increments('id');
+        $table->integer('user_id')->unsigned();
+        $table->foreign('user_id')->references('id')->on('utilisateurs');
+        $table->mediumText('message');
+         $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('logs');
+    }
+}
