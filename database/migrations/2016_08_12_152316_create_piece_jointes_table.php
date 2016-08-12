361e10686d615d073d302183b4a5b622df8ab7d9
Younes Khoubza <younes.khoubza@gmail.com>
2016-08-12 16:30:12 +0100

    piece jointes table

---
+++ database/migrations/2016_08_12_152316_create_piece_jointes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePieceJointesTable extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+//        id, dispay_name, link, type, size, owner_type, owner_id
+        Schema::create('piece_jointes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('display_name');
+            $table->string('link');
+            $table->string('type');
+            $table->double('size');
+
+            $table->morphs('owner');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::drop('piece_jointes');
+    }
+}
