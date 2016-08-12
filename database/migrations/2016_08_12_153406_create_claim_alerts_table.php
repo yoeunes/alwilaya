f666252de19f63893196b020bf79bbffd35f24ff
Younes Khoubza <younes.khoubza@gmail.com>
2016-08-12 16:37:36 +0100

    claim alerts table

--- database/migrations/2016_08_12_153406_create_claim_alerts_table.php
+++ database/migrations/2016_08_12_153406_create_claim_alerts_table.php
@@ -3,21 +3,19 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePieceJointesTable extends Migration {
+class CreateClaimAlertsTable extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
     public function up() {
-        Schema::create('piece_jointes', function (Blueprint $table) {
+        Schema::create('claim_alerts', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('display_name');
-            $table->string('link');
-            $table->string('type');
-            $table->double('size');
+            $table->text('message');
+            $table->integer('claimtype_id')->unsigned();
 
-            $table->morphs('owner');
+            $table->foreign('claimtype_id')->references('id')->on('claim_types');
 
             $table->timestamps();
         });
@@ -29,6 +27,6 @@ class CreatePieceJointesTable extends Migration {
      * @return void
      */
     public function down() {
-        Schema::drop('piece_jointes');
+        Schema::drop('claim_alerts');
     }
 }
