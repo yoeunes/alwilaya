9c39cd76e3017805844356ec571cd263e7267893
Younes Khoubza <younes.khoubza@gmail.com>
2016-08-16 05:13:46 +0100

    claims table

---
+++ database/migrations/2016_08_16_040658_create_claims_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClaimsTable extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('claims', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('objet');
+            $table->text('message');
+            $table->boolean('status');
+            $table->timestamp('date_envoi');
+            $table->timestamp('date_reponse');
+            $table->string('num_rec')->unique();
+            $table->boolean('deleted');
+
+            $table->integer('citizen_id')->unsigned();
+            $table->foreign('citizen_id')->references('id')->on('citizens');
+
+            $table->integer('city_id')->unsigned();
+            $table->foreign('city_id')->references('id')->on('cities');
+
+            $table->integer('citySub_id')->unsigned();
+            $table->foreign('citySub_id')->references('id')->on('city_subs');
+
+            $table->integer('cityMap_id')->unsigned();
+            $table->foreign('cityMap_id')->references('id')->on('city_maps');
+
+            $table->integer('claimType_id')->unsigned();
+            $table->foreign('claimType_id')->references('id')->on('claim_types');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::drop('claims');
+    }
+}
