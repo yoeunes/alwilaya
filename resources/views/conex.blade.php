71820b1f1d65618107bc736c44dc80afbd5e6640
fatima ezzahra hadadi <fatima.ezzahrae.hadadi@gmail.com>
2016-08-17 13:50:40 +0200

    -ales vues d'authentification

--- resources/views/conex.blade.php
+++ resources/views/conex.blade.php
@@ -1,33 +1,45 @@
 @extends('templateConex')
 @section('contenu')
 
-    <div class="col-sm-offset-4 col-sm-3">
 
-
-        <div  class="panel panel-default   " >
+    @if ($errors->count() > 0)
+        <div class="alert alert-danger">
+            <strong>whoops  vos avez les erreurs suivants !!.</strong>
+            <ul>
+                @foreach($errors ->all() as $error)
+                    <li> {{$error}} </li>
+                @endforeach
+            </ul>
+        </div>
+@endif
+        <div class="col-sm-offset-4 col-sm-3">
+        <div  class="panel panel-default " >
         <div  class="panel-heading  text-center"><strong>se connecter</strong></div>
         <div class="panel-body">
             <div class= "imgcontainer ">
                 <img src=" img_avatar2.png"  class="img-circle"  width="260" height="236">
             </div>
-        <form>
+        <form method="post" action="/connexion">
+            <input type="hidden" name="_token" value="{{  csrf_token() }}">
             <div class="form-group ">
-                <label >Adresse électronique:</label>
-              <input type="email" class="form-control" id="email" placeholder="email">
-            </div>
-            <div class="form-group">
-                <label >Mot de passe:</label>
-                <input type="password"  placeholder="mot de passe" class="form-control" >
-            </div>
-            <div class="checkbox">
-                <label><input type="checkbox">garder ma session active</label>
-            </div>
+                <label for="email">Adresse électronique:</label>
+              <input type="email" name="email" class="form-control" id="email" placeholder="email">
 
-            <button type="submit" class="form-control  btn btn-success" >Connexion</button>
-             <div class="btn-link  text-center  well well-sm "><a href="#"> Mot de passe oublié ?</a></div>
+          </div>
+          <div class="form-group">
+              <label for="password">Mot de passe:</label>
+              <input type="password" name="password" placeholder="mot de passe"  class="form-control" >
 
-            </form>
-    </div>
-    </div>
-    </div>
-    @endsection
+           </div>
+<div class="checkbox">
+  <label><input type="checkbox" name="rememberme">garder ma session active</label>
+</div>
+
+<button type="submit" class="form-control  btn btn-success" >Connexion</button>
+<div class="btn-link  text-center  well well-sm "><a href="#"> Mot de passe oublié ?</a></div>
+
+</form>
+</div>
+</div>
+</div>
+@endsection
--- resources/views/templateConex.blade.php
+++ resources/views/templateConex.blade.php
@@ -8,58 +8,9 @@
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <title>templateConex</title>
-    <style>
-
-        input[type=text], input[type=password] {
-            width: 100%;
-            padding: 12px 20px;
-            margin: 8px 0;
-            display: inline-block;
-            border: 1px solid #ccc;
-            box-sizing: border-box;
-        }
-
-        button {
-            background-color: #4CAF50;
-            color: white;
-            padding: 14px 20px;
-            margin: 8px 0;
-            border: none;
-            cursor: pointer;
-            width: 100%;
-        }
-
-        .cancelbtn {
-            width: auto;
-            padding: 10px 18px;
-            background-color: #f44336;
-        }
-
-        .imgcontainer {
-            text-align: center;
-            margin: 24px 0 12px 0;
-        }
-
-        img.avatar {
-            width: 40%;
-            border-radius: 50%;
-        }
-
-        .container {
-            padding: 16px;
-        }
-
-        span.psw {
-            float: right;
-            padding-top: 16px;
-        }
-
-
-    </style>
-</head>
 <body>
 @yield('contenu')
+
 </body>
 </html>
 
